10128fa956c97b1562f98614bfe7fe929e944fee
Elena <elena1709@yandex.ru>
2022-10-10 11:42:49 +0300

    commit for hw

---
+++ 23/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на
+// вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void Kub(int n)
+{
+    for (int i = 1; i <= n; i++)
+
+    {
+        Console.Write($"{i*i*i}");
+        if (i !=n)
+        {
+            Console.Write(", ");     
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите число:  ");
+int N = int.Parse(Console.ReadLine()!);
+Kub(N);
